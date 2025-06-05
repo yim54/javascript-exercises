85d52ee5df9c7daf9d96a08c7b484aa9299b963b
yim54 <143656868+yim54@users.noreply.github.com.>
2025-06-04 17:25:15 -0700

    Complete 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    return array.filter(item => !args.includes(item));
 };
 
 // Do not edit below this line
