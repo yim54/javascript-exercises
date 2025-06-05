a7562579f65c3fee3feff4ec9b656d9e8d18252e
yim54 <143656868+yim54@users.noreply.github.com.>
2025-06-04 17:32:44 -0700

    Complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // check for non-integer parameters
 
+    if (!(Number.isInteger(a)) || !(Number.isInteger(b))) {
+        return "ERROR";
+    }
+
+    // check for negative numbers
+
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+
+    // swap a and b if b is larger
+
+    if (a > b) {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+
+    // sum
+
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
