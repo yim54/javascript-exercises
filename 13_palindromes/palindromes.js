614bbdf5d747de705ed78a313360cf16f02ed146
yim54 <143656868+yim54@users.noreply.github.com.>
2025-06-10 12:38:03 -0700

    Complete 13_palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,31 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    // string preprocessing
 
+    word = word.toLowerCase();
+    word = word.replaceAll(" ", "");
+
+    let i = 0;
+    let j = word.length - 1;
+    while (j > i) {
+        // check for alphanumeric
+
+        let regex = /^[a-zA-Z0-9]+$/;
+        if (!regex.test(word[i])) {
+            i++;
+        }
+
+        if (!regex.test(word[j])) {
+            j--;
+        }
+
+        console.log(word[i]+ word[j]);
+        if (word[i] != word[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
 };
 
 // Do not edit below this line
