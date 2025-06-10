23e6146d670f4b70d20086a4948f87003c3c3af8
yim54 <143656868+yim54@users.noreply.github.com.>
2025-06-10 13:12:17 -0700

    16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+function age(person) {
+    if (!("yearOfDeath" in person)) {
+        let d = new Date();
+        person.yearOfDeath = d.getFullYear();
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        if (age(oldest) < age(person)) {
+            return person;
+        }
+        return oldest;
+    })
 };
 
 // Do not edit below this line
