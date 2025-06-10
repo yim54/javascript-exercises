b1a40b7a7530a5f49cc54b918b75f70fb4b7d3e3
yim54 <143656868+yim54@users.noreply.github.com.>
2025-06-10 12:49:43 -0700

    Complete 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    // convert strings to number
 
+    num = Number(num);
+    if (num == NaN) {
+        return "OOPS";
+    }
+
+    // check for negative number
+
+    if (num < 0) {
+        return "OOPS";
+    }
+
+    if (num == 0) {
+        return 0;
+    }
+
+    // get fib number
+
+    let num1 = 1;
+    let num2 = 1;
+    for (let i = 2; i < num; i++) {
+        let next = num1 + num2;
+        num1 = num2;
+        num2 = next;
+    }
+    return num2;
 };
 
 // Do not edit below this line
